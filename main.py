4111a6c48eef518dbd7f9cfe3c0a325607c25140
SergDzhus <SDzhoos@gmail.com>
2024-10-24 12:48:23 +0300

    Добавлен тестовый файл.

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def show_history():
     with open(history_file, 'r') as f:
         history = json.load(f)
         for item in history:
-            files_listbox.insert(END, item['filepath'])
-            linkes_listbox.insert(END, item['dowload_link'])
+            files_listbox.insert(END, item['file_path'])
+            linkes_listbox.insert(END, item['download_link'])
 
 def save_history(file_path, download_link):
     history = []
