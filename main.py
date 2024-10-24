cb2f5c892f9d33a069f3489ad5142d705f2b48a3
SergDzhus <SDzhoos@gmail.com>
2024-10-24 12:16:41 +0300

    Добавлена ф-ция сохранения ссылок в формате json.

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from tkinter import ttk
 from tkinter import messagebox as mb
 import requests
 import pyperclip
+import json
+import os
+
+history_file = "upload_history.json"
+
+
+def save_history(file_path, download_link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, 'r') as f:
+            history = json.load(f)
+    history.append({"file_path": os.path.basename(file_path), "download_link": download_link})
+    with open(history_file, 'w') as f:
+        json.dump(history, f, indent=4)
 
 
 def upload():
@@ -18,6 +32,7 @@ def upload():
                 entry.delete(0, END)
                 entry.insert(0, download_link)
                 pyperclip.copy(download_link)
+                save_history(filepath, download_link)
                 mb.showinfo("Ссылка скопирована!",f"Ссылка {download_link} успешно скопирована в буфер обмена!")
     except Exception as e:
         mb.showerror("Ошибка!",f"Произошла ошибка {e}!")
