97d293a5a3e063c47ade2aef3bf50d1306a331e8
SergDzhus <SDzhoos@gmail.com>
2024-10-24 12:02:29 +0300

    Добавлено автоматическое сохранение в буфер обмена.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import filedialog as fd
 from tkinter import ttk
 from tkinter import messagebox as mb
 import requests
+import pyperclip
 
 
 def upload():
@@ -16,6 +17,8 @@ def upload():
                 download_link = response.json()['link']
                 entry.delete(0, END)
                 entry.insert(0, download_link)
+                pyperclip.copy(download_link)
+                mb.showinfo("Ссылка скопирована!",f"Ссылка {download_link} успешно скопирована в буфер обмена!")
     except Exception as e:
         mb.showerror("Ошибка!",f"Произошла ошибка {e}!")
 
